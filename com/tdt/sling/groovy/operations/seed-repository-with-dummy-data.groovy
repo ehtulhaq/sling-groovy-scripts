3f718d06fd57061bde62dba6a6f3868eee427f0e
Ehtesham Mohammed <ehtulhaq65@gmail.com>
2019-09-23 00:13:26 -0400

    Update seed-repository-with-dummy-data.groovy

--- com/tdt/sling/groovy/operations/seed-repository-with-dummy-data.groovy
+++ com/tdt/sling/groovy/operations/seed-repository-with-dummy-data.groovy
@@ -1,2 +1,17 @@
 package tdt.sling.groovy.operations
 
+import org.apache.sling.api.resource.Resource
+import org.apache.sling.api.resource.ResourceResolver
+import org.apache.sling.api.resource.ResourceResolverFactory
+
+ResourceResolverFactory resourceResolverFactory = osgi.getService(ResourceResolverFactory.class)
+ResourceResolver resourceResolver = resourceResolverFactory.getServiceResourceResolver(null)
+
+
+def count = 100
+def destinationPath = '/content/tdt/site'
+Resource destinationResource = resourceResolver.getResource(destinationPath)
+for (int i = 0; i < count; i++) {
+    resourceResolver.create(destinationResource, "${i}-tdt-sample", ['jcr:primaryType': 'nt:unstructured'])
+}
+resourceResolver.commit()
